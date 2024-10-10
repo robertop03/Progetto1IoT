8bcf471f66f8b6f03f2fbf35840e212be3d7f215
Sohail <151531157+mamax2@users.noreply.github.com>
2024-10-10 21:58:19 +0200

    maybe cooked the best code of my life

--- Src/Main/Main.ino
+++ Src/Main/Main.ino
@@ -1,10 +1,10 @@
 #include <LiquidCrystal.h>
 #include <avr/sleep.h>
-
+#include <stdlib.h>
 
 #define LED1_PIN 10
 #define LED2_PIN 11
-#define LED3_PIN 12 
+#define LED3_PIN 12
 #define LED4_PIN 13
 #define LEDS_PIN 9
 #define BUT1_PIN A1
@@ -14,68 +14,173 @@
 #define POT_PIN A5
 
 int score;
-
+int numero;
+char binarioString[5];
+unsigned long startMillis;
+unsigned long currentMillis;
+int timelimit;
+int difficulty;
+const int ledPins[] = {LED1_PIN, LED2_PIN, LED3_PIN, LED4_PIN};    // L1, L2, L3, L4 (binary LEDs)
+const int buttonPins[] = {BUT1_PIN, BUT2_PIN, BUT3_PIN, BUT4_PIN}; // B1, B2, B3, B4 (buttons for binary LEDs)
 
 // Create an LCD object. Parameters: (RS, E, D4, D5, D6, D7):
 LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
 
-
-int generateRandomicNumber() {
-  return random(0, 16);  // random(min, max) generate and return a number between min and max-1
+int generateRandomicNumber()
+{
+  return random(0, 16); // random(min, max) generate and return a number between min and max-1
 }
 
-void wakeUpNow(){
-  
+// Function to reset LEDs
+void resetLeds()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(ledPins[i], LOW);
+    ledState[i] = LOW;
+  }
 }
 
-void sleepNow(){
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); 
-  attachInterrupt(0,wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function wakeUpNow when pin 2 gets LOW
+void wakeUpNow() {}
+
+void sleepNow()
+{
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  attachInterrupt(0, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function wakeUpNow when pin 2 gets LOW
   sleep_mode();
   sleep_disable();
   detachInterrupt(0);
 }
 
-void setup() {
+void setup()
+{
   // Initialization to generate random number between 0 and 15
   Serial.begin(9600);
   randomSeed(analogRead(0));
   lcd.begin(16, 2);
+  // Setup pins
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(ledPins[i], OUTPUT);
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+  pinMode(LEDS_PIN, OUTPUT);
+  pinMode(POT_PIN, INPUT);
   score = 0;
   initGame();
 }
 
-void loop() {
-  // How to use generateRandomicNumber
-  // int numero = generateRandomicNumber();
-  
+// Function to check if binary is correct
+bool checkBinary()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    if (ledState[i] != ((randomNum >> i) & 1))
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
-void initGame() {
-  
-  int fadeAmount = 5;
+void initGame()
+{
+  int fadeAmount = 10;
   int currIntensity = 0;
-  int buttonState;
+  int buttonState = digitalRead(BUT1_PIN);
   lcd.setCursor(0, 0);
   lcd.write("Welcome to GMB!");
   lcd.setCursor(0, 1);
   lcd.write("Press B1 to Strt");
-
-  while(1){
-    buttonState = digitalRead(BUT1_PIN);
-    analogWrite(LEDS_PIN, currIntensity);   
+  bool endInit = true;
+  while (endInit)
+  {
+    analogWrite(LEDS_PIN, currIntensity);
     currIntensity = currIntensity + fadeAmount;
-    if (currIntensity == 0 || currIntensity == 255) {
-      fadeAmount = -fadeAmount ; 
-    }     
-
-    if (buttonState == LOW)  {
-      break;
+    if (currIntensity == 0 || currIntensity == 255)
+    {
+      fadeAmount = -fadeAmount;
     }
 
-    delay(20); 
-
+    if (buttonState == HIGH)
+    {
+      endInit = false;
+      lcd.clear();
+      game();
+    }
+    delay(20);
   }
+}
 
+void game()
+{
+  int but1 = digitalRead(BUT1_PIN);
+  int but2 = digitalRead(BUT2_PIN);
+  int but3 = digitalRead(BUT3_PIN);
+  int but4 = digitalRead(BUT4_PIN);
+  int potRead = analogRead(POT_PIN);
+  bool gameActive = false;
 
+  if (!gameActive)
+  {
+    if (digitalRead(but1) == LOW)
+    {
+      gameActive = true;
+      resetLeds();
+      lcd.setCursor(0, 0);
+      lcd.write("Go!");
+      difficulty = map(potRead, 0, 1023, 1, 4); // Map potentiometer value to difficulty
+      timeLimit = 5000 / difficulty;            // Adjust time based on difficulty
+      delay(2000);
+    }
+  }
+  else
+  {
+    randomNum = generateRandomNumber();
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.write("Number:%d " randomNum);
+
+    long startTime = millis();
+    bool correct = false;
+
+    while (millis() - startTime < timeLimit)
+    {
+      for (int i = 0; i < 4; i++)
+      {
+        if (digitalRead(buttonPins[i]) == LOW)
+        {
+          ledState[i] = !ledState[i];
+          digitalWrite(ledPins[i], ledState[i]);
+          delay(200); // Debounce delay
+        }
+      }
+
+      if (checkBinary())
+      {
+        correct = true;
+        break;
+      }
+    }
+
+    if (correct)
+    {
+      score++;
+      displayMessage("GOOD! Score: " + String(score));
+      delay(2000);
+    }
+    else
+    {
+      digitalWrite(redLedPin, HIGH);
+      delay(1000);
+      digitalWrite(redLedPin, LOW);
+      displayMessage("Game Over - Final Score: " + String(score));
+      delay(5000);
+      showWelcome();
+      gameActive = false;
+    }
+
+    // Reduce time for the next round
+    timeLimit = timeLimit - timeLimit * 0.1;
+  }
 }
